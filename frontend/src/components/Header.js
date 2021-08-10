140a8b6e548ec9576fcd3e62d175ab4a407dbe7d
simon <simon.bergsten@hotmail.com>
2021-08-10 11:05:30 +0200

    Added state component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap'
+
+const navbarStype = {
+    backgroundColor: 'lightblue'
+}
+
+const Header = (props) => {
+    return (
+        <Navbar style={navbarStype}  variant="light">
+            <Container>
+                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+};
+
+export default Header;
