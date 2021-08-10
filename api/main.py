f2b24288f73b1e7109839dc695ec357b22b8ba57
simon <simon.bergsten@hotmail.com>
2021-08-10 18:01:07 +0200

    added flask

--- api/main.py
+++ api/main.py
@@ -1,5 +1,10 @@
 # from fastapi import FastApi 
 from flask import Flask
+import requests
+
+UNSPLASH_URL = ""
+UNSPLASH_KEY = ""
+
 
 app = Flask(__name__)
 
@@ -7,6 +12,11 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route("/new-image")
+def new_image():
+    word = requests.args.get("query")
+    return {"word":word}
+
+
 if __name__=="__main__":
     app.run(host="0.0.0.0", port = 5050)
-    
