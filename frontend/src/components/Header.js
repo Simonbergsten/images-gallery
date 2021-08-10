791fe1358c48b14e82c914e9bb44d35dfe7618c3
simon <simon.bergsten@hotmail.com>
2021-08-10 13:30:13 +0200

    save images in the state

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStype = {
-  backgroundColor: "lightblue",
+  backgroundColor: 'lightblue',
 };
 
 const Header = (props) => {
